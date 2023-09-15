8bd4c49fd3e7b2e55e28820f69b6cf98fc032df5
kaeru2193 <kaeru2193@gmail.com>
2023-09-15 19:58:26 +0900

    雰算字体系に対応。

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 const BigNumber = require("bignumber.js");
+const PhunNum = {
+    "0": "〇",
+    "1": "〡",
+    "2": "〢",
+    "3": "〣",
+    "4": "〤",
+    "5": "〥",
+    "6": "〦",
+    "7": "〧",
+    "8": "〨",
+    "9": "〩",
+    "a": "〹",
+    "b": "〺",
+    ".": "・",
+};
 const convertButton = document.getElementById('convert');
 convertButton.addEventListener("click", () => {
+    const phunNumToggle = document.getElementById('phunnum');
+    const isPhun = phunNumToggle.checked;
     const inputNum = document.getElementById('number');
     const stringToConvert = String(inputNum.value);
     const inputBase = document.getElementById('beforebase');
@@ -14,6 +31,11 @@ convertButton.addEventListener("click", () => {
         BigNumber.config({ DECIMAL_PLACES: numberOfDigits });
         const num = BigNumber(stringToConvert, beforeBase);
         converted = num.toString(toConvertBase);
+        if (isPhun) {
+            converted = converted.split("").map(e => {
+                return PhunNum[e];
+            }).join("");
+        }
     }
     catch (_a) {
         converted = "エラー";
--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
 const BigNumber = require("bignumber.js")
 
+const PhunNum = {
+    "0": "〇",
+    "1": "〡",
+    "2": "〢",
+    "3": "〣",
+    "4": "〤",
+    "5": "〥",
+    "6": "〦",
+    "7": "〧",
+    "8": "〨",
+    "9": "〩",
+    "a": "〹",
+    "b": "〺",
+    ".": "・",
+}
+
 const convertButton: HTMLInputElement = <HTMLInputElement> document.getElementById('convert')
 
+
 convertButton.addEventListener("click", () => {
+    const phunNumToggle: HTMLInputElement = <HTMLInputElement> document.getElementById('phunnum')
+    const isPhun = phunNumToggle.checked
+
     const inputNum = <HTMLInputElement> document.getElementById('number')
     const stringToConvert: string = String(inputNum.value)
 
@@ -21,6 +41,11 @@ convertButton.addEventListener("click", () => {
         BigNumber.config({ DECIMAL_PLACES: numberOfDigits });
         const num = BigNumber(stringToConvert, beforeBase)
         converted = num.toString(toConvertBase)
+        if (isPhun) {
+            converted = converted.split("").map(e => {
+                return PhunNum[e]
+            }).join("")
+        }
     } catch {
         converted = "エラー"
     }
